2c8f1ece98a4beef085f1a176342f00e3f9b9d85
Spencer Baird <spencer.baird36@gmail.com>
2025-09-07 12:05:33 -0400

    Update API URL for production deployment

--- the-league/src/config/api.ts
+++ the-league/src/config/api.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://the-league-api-1ff2960f0715.herokuapp.com' 
+  ? 'https://the-league-f8fa1bccd03a.herokuapp.com' 
   : 'http://localhost:5000';
 
 export const apiRequest = async (endpoint: string, options?: RequestInit) => {
